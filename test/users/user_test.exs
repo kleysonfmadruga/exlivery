e7770a913f6eda5da4cbe195b401f32f5c733b3c
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-07-24 19:16:53 -0300

    Adds tests for User.build/5

---
+++ test/users/user_test.exs
@@ -0,0 +1,33 @@
+defmodule Exlivery.Users.UserTest do
+  use ExUnit.Case
+
+  alias Exlivery.Users.User
+
+  import Exlivery.Factory
+
+  describe "build/5" do
+    test "when all params are valid, returns a user" do
+      response = User.build("12345678900", "Jose", "jose@gmail.com", 20, "Rua dos bobos")
+
+      expected_response = {:ok, build(:user)}
+
+      assert expected_response == response
+    end
+
+    test "when the age is lower than 18 years, returns an error" do
+      response = User.build("12345678900", "Jose", "jose@gmail.com", 17, "Rua dos bobos")
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert expected_response == response
+    end
+
+    test "when the CPF isn't a bitstring, returns an error" do
+      response = User.build(12_345_678_900, "Jose", "jose@gmail.com", 20, "Rua dos bobos")
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert expected_response == response
+    end
+  end
+end
