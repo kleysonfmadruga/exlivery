00433b37016ea1ae8bbb4913f7ac513f4355d525
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-07-24 12:13:28 -0300

    Adds item_factory/0 function in Exlivery.Factory module

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,8 +1,9 @@
 defmodule Exlivery.Factory do
   @moduledoc false
-  
+
   use ExMachina
 
+  alias Exlivery.Orders.Item
   alias Exlivery.Users.User
 
   def user_factory do
@@ -12,6 +13,15 @@ defmodule Exlivery.Factory do
       cpf: "12345678900",
       email: "jose@gmail.com",
       name: "Jose"
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Açaí",
+      category: :sobremesa,
+      quantity: 1,
+      unit_price: Decimal.new("10.99")
     }
   end
 end
