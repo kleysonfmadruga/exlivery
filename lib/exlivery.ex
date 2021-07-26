61a930c117965cd84f61c5682137f32e6e368a6a
kleysonfmadruga <kleysonfmadruga26@gmail.com>
2021-07-26 20:02:11 -0300

    Adds function delegations to Orders and Users Upsert.call function

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,5 +1,16 @@
 defmodule Exlivery do
-  def hello do
-    :world
+  @moduledoc false
+
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Orders.Upsert, as: OrderUpsert
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.Upsert, as: UserUpsert
+
+  def start_agents do
+    UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
+
+  defdelegate upsert_user(params), to: UserUpsert, as: :call
+  defdelegate upsert_order(params), to: OrderUpsert, as: :call
 end
